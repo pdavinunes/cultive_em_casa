7ce92e05a144cff3e12b325d082a11ded8706cac
pdavinunes <p.davi.nunes@gmail.com>
2020-05-22 20:48:50 -0300

    Rota POST para users feita

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -7,11 +7,27 @@ module.exports = {
 
         conexao.query(sql, function(error, results, fields){
             if(error) return response.json(error)
+            conexao.end()
             return response.json(results)
         })
     },
     
     async store(request, response){
+        const {username, email, password} = request.body
+
+        console.log(username, email, password);
+        const sql = `INSERT INTO 
+                        users (username, email, password) 
+                    VALUES ('${username}', '${email}', '${password}')`
+        
+        conexao.query(sql, function(error, results, fields){
+            if(error) {
+                console.log(error)
+                return response.json(error.json)
+            }
+            conexao.end();
+            return response.json({status: '201'})
+        })
 
     },
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const UserController = require('./controllers/UserController')
 const routes = Router()
 
 routes.get('/users', UserController.index)
+routes.post('/users', UserController.store)
 
 routes.get('/', (req, res) => {
     return res.json({status : 'running'})
